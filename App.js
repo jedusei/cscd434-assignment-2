f6e9add4b7b9387956a3c7f2f54fd7c21de3114a
Joseph Edusei <jedusei99@gmail.com>
2021-07-13 17:17:05 +0000

    implemented app screen switching functionality.

--- App.js
+++ App.js
@@ -1,21 +1,49 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+
+const screens = ["Screen 1", "Screen 2", "Screen 3"];
 
 export default function App() {
+  const [currentScreen, setCurrentScreen] = useState(0);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+      <View style={{ flex: 1 }} />
+      <View style={{ flexDirection: 'row', justifyContent: 'space-evenly', paddingVertical: 20 }}>
+        {screens.map((screen, i) => {
+          const isSelected = (i == currentScreen);
+          return (
+            <TouchableOpacity key={screen} onPress={() => setCurrentScreen(i)}>
+              <View style={Object.assign({}, styles.chip, isSelected && styles.selectedChip)}>
+                <Text style={isSelected ? styles.selectedChipText : styles.chipText}>{screen}</Text>
+              </View>
+            </TouchableOpacity>
+          );
+        })}
+      </View>
+    </View >
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#f2f8fd'
   },
+  chip: {
+    backgroundColor: "#fefefe",
+    paddingHorizontal: 15,
+    paddingVertical: 10,
+    borderRadius: 30
+  },
+  chipText: {
+    color: '#000'
+  },
+  selectedChip: {
+    backgroundColor: "#575e94"
+  },
+  selectedChipText: {
+    color: "#fff",
+    fontWeight: 'bold'
+  }
 });
